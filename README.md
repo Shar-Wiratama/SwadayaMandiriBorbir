bb852f89d096686e1a3ef2cfd3fcebf951e38236
Shar-Wiratama <Shar-Wiratama>
2023-08-20 19:33:38 +0700

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# SwadayaMandiriBorbir
-Tugas akhir 
---
+++ android/app/src/main/kotlin/com/example/swadaya_mandiri_borbir/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.swadaya_mandiri_borbir
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
